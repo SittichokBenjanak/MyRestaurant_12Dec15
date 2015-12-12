e07c854a438605a906a65ff09eee382cf2e006f6
mos <moszajang@outlook.com>
2015-12-12 13:39:15 +0700

    Downgrad เรียบร้อยแล้ว

--- app/src/main/java/drucc/sittichok/myrestaurant/ManageTABLE.java
+++ app/src/main/java/drucc/sittichok/myrestaurant/ManageTABLE.java
@@ -1,5 +1,6 @@
 package drucc.sittichok.myrestaurant;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -10,7 +11,18 @@ public class ManageTABLE {
 
     //Explicit
     private MyOpenHelper objMyOpenHelper;
-    private SQLiteDatabase WriteSqLiteDatabase,reidSqLiteDatabase;
+    private SQLiteDatabase WriteSqLiteDatabase, reidSqLiteDatabase;
+
+    public static final String TABLE_USER = "userTABLE";
+    public static final String COLUMN_id_ = "_id";
+    public static final String COLUMN_User = "User";
+    public static final String COLUMN_Password = "Password";
+    public static final String COLUMN_Name = "Name";
+
+    public static final String TABLE_FOOD = "foodTABLE";
+    public static final String COLUMN_Food_ = "Food";
+    public static final String COLUMN_Source = "Source";
+    public static final String COLUMN_Price = "Price";
 
 
     public ManageTABLE(Context context) {
@@ -22,5 +34,26 @@ public class ManageTABLE {
 
     }   // Constructor
 
+    public long addValueToUser(String strUser, String strPassword, String strName) {
+
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(COLUMN_User,strUser);
+        objContentValues.put(COLUMN_Password,strPassword);
+        objContentValues.put(COLUMN_Name,strName);
+
+        return WriteSqLiteDatabase.insert(TABLE_USER,null,objContentValues);
+    }
+
+    public long addValueToFood(String strFood,String strSource,String strPrice) {
+
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(COLUMN_Food_,strFood);
+        objContentValues.put(COLUMN_Source,strSource);
+        objContentValues.put(COLUMN_Price,strPrice);
+
+
+        return WriteSqLiteDatabase.insert(TABLE_FOOD,null,objContentValues);
+
+    }
 
 }   // Main Class
